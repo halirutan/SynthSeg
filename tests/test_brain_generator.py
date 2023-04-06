337d1cdd931f633bd31669b29ec55b0922b70af6
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-04-06 18:14:58 +0200

    add random seed brain generator test + test files

--- tests/test_brain_generator.py
+++ tests/test_brain_generator.py
@@ -1,14 +1,29 @@
-import tensorflow as tf
 from . import TestData
 import SynthSeg.brain_generator as bg
+import numpy as np
+import tensorflow as tf
+from pathlib import Path
+import os
 
 
 def test_brain_generator():
-    tf.config.run_functions_eagerly(True)
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+    image_path = Path(__file__).parent / "brain_image_seed43.npy"
+    label_path = Path(__file__).parent / "brain_label_seed43.npy"
+
+    tf.random.set_seed(43)
+    np.random.seed(43)
+
     label_map_files = TestData.get_label_maps()
-    brain_generator = bg.BrainGenerator(label_map_files[0])
-    im, lab = brain_generator.generate_brain()
-    assert im.shape == lab.shape, "Shape of the label image and the generated MRI are not the same"
 
+    brain_generator = bg.BrainGenerator(label_map_files[0], output_shape=16)
+    image, label = brain_generator.generate_brain()
 
+    # np.save(image_path, image)
+    # np.save(label_path, label)
 
+    target_image = np.load(str(image_path))
+    target_label = np.load(str(label_path))
+
+    assert np.allclose(image, target_image)
+    assert np.allclose(label, target_label)
