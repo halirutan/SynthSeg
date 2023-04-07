45630996ad659b864156067b1ffa37359b2058c7
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2023-04-08 00:32:35 +0200

    add random_spatial_deformation test

--- tests/ext/lab2im/test_layers.py
+++ tests/ext/lab2im/test_layers.py
@@ -1,4 +1,4 @@
-from ext.lab2im.layers import RandomCrop
+from ext.lab2im import layers
 import numpy as np
 from keras.models import Model
 from keras.layers import Input
@@ -9,7 +9,7 @@ def test_random_crop(model_inputs, set_random_seeds):
     random_crop_target_path = Path(__file__).parent / "random_crop_seed43.npy"
 
     labels_input = Input(shape=model_inputs[0].shape[1:], name='labels_input', dtype='int32')
-    random_crop_layer = RandomCrop(crop_shape=[16, 16, 16])(labels_input)
+    random_crop_layer = layers.RandomCrop(crop_shape=[16, 16, 16])(labels_input)
 
     model = Model(inputs=labels_input, outputs=random_crop_layer)
 
@@ -18,6 +18,30 @@ def test_random_crop(model_inputs, set_random_seeds):
     out_target = np.load(str(random_crop_target_path))
 
     assert np.allclose(out, out_target)
+
+
+def test_random_spatial_deformation(model_inputs, set_random_seeds):
+    random_spatial_deformation_target_path = Path(__file__).parent / "random_spatial_deformation_seed43.npy"
+
+    labels_input = Input(shape=model_inputs[0].shape[1:], name='labels_input', dtype='int32')
+
+    random_spatial_deformation_layer = layers.RandomSpatialDeformation(
+        scaling_bounds=0.2,
+        rotation_bounds=15,
+        shearing_bounds=0.012,
+        nonlin_std=3.0,
+        nonlin_scale=0.04,
+        inter_method="nearest",
+    )(labels_input)
+
+    model = Model(inputs=labels_input, outputs=random_spatial_deformation_layer)
+
+    output = model.predict(model_inputs[0])
+
+    output_target = np.load(str(random_spatial_deformation_target_path))
+
+    assert np.allclose(output, output_target)
+
 
 
 
