9ba459596ee685de7cdb3ea5d397484e036a1a05
David Carreto Fidalgo <david.carreto.fidalgo@mpcdf.mpg.de>
2024-08-01 10:26:56 +0200

    add delete option

--- scripts/slurm/generation/tfrecord_to_nifti.py
+++ scripts/slurm/generation/tfrecord_to_nifti.py
@@ -24,12 +24,20 @@ class Options:
     skip: int = 0
     # Tar the images and labels into one tar file. In each TFRecord file there are 8 image/label pairs.
     tar: bool = False
+    # Delete the TFRecord file after converting it to NifTIs?
+    delete: bool = False
 
 
-def to_niftis(tfrecord_path: Path, brain_generator_config: Path, output_path: Path, tar: bool = False):
+def to_niftis(
+    tfrecord_path: Path,
+    brain_generator_config: Path,
+    output_path: Path,
+    tar: bool = False,
+    delete: bool = False,
+):
     """Convert a TFRecord file into NIfTI files.
 
-    Each volume in the TFRecord file is appended by `_{i}_volume.nii.gz` and the labels by `_{i}_labels.nii.gz`.
+    Each image in the TFRecord file is appended by `_{i}_image.nii.gz` and the labels by `_{i}_labels.nii.gz`.
 
     Args:
         tfrecord_path: Path to the TFRecord file.
@@ -63,7 +71,7 @@ def to_niftis(tfrecord_path: Path, brain_generator_config: Path, output_path: Pa
             image,
             brain_generator.aff,
             brain_generator.header,
-            str(output_path / f"{tfrecord_path.stem}_{i}_volume.nii.gz"),
+            str(output_path / f"{tfrecord_path.stem}_{i}_image.nii.gz"),
         )
         save_volume(
             labels,
@@ -71,21 +79,26 @@ def to_niftis(tfrecord_path: Path, brain_generator_config: Path, output_path: Pa
             brain_generator.header,
             str(output_path / f"{tfrecord_path.stem}_{i}_labels.nii.gz"),
         )
-    
+
     if tar:
         with tarfile.open(f"{output_path / tfrecord_path.stem}.tar", "w") as tarf:
-            for file in sorted(list(output_path.glob(f"{tfrecord_path.stem}_*.nii.gz"))):
+            for file in sorted(
+                list(output_path.glob(f"{tfrecord_path.stem}_*.nii.gz"))
+            ):
                 tarf.add(file, arcname=file.name)
                 file.unlink()
+
+    if delete:
+        tfrecord_path.unlink()
 
 
 if __name__ == "__main__":
     opts = simple_parsing.parse(Options)
     if opts.input.is_file():
-        to_niftis(opts.input, opts.config, opts.output, opts.tar)
+        to_niftis(opts.input, opts.config, opts.output, opts.tar, opts.delete)
     else:
         file_list = sorted(list(opts.input.glob("*.tfrecord")))
         for i, file in tqdm(enumerate(file_list)):
             if i < opts.skip:
                 continue
-            to_niftis(file, opts.config, opts.output, opts.tar)
+            to_niftis(file, opts.config, opts.output, opts.tar, opts.delete)
